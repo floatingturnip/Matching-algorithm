a8276c998841a394c140ff7497af2d59edfe10f7
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2021-03-01 21:23:27 -0800

    New structure for random_matcher.py

--- random_matcher.py
+++ random_matcher.py
@@ -1,20 +1,32 @@
 import random
 import csv
 
-filename = 'names.txt'
+def open_file(filename = 'names.txt'):
 
-with open(filename) as f:
-    content = f.readlines()
-# removes the "\n"
-content = [x.strip() for x in content]
-print(content)
-random.shuffle(content)
-print(content)
+    with open(filename) as f:
+        content = f.readlines()
+    # removes the "\n"
+    content = [x.strip() for x in content]
+    return content
 
-n = 2 # number of people per group
-subgroups = [content[i * n:(i + 1) * n] for i in range((len(content) + n - 1) // n)]
-print(subgroups)
 
-with open("out.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerows(subgroups)
+def random_match(full_list, n=2):
+    print(full_list)
+    random.shuffle(full_list)
+    print(full_list)
+
+    subgroups = [full_list[i * n:(i + 1) * n] for i in range((len(full_list) + n - 1) // n)]
+    print(subgroups)
+    return subgroups
+
+
+def write_groups_to_csv(subgroups):
+    with open("out.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(subgroups)
+
+
+if __name__ == "__main__":
+    full_list = open_file()
+    subgroups = random_match(full_list)
+    write_groups_to_csv(subgroups)
